bad6db16ff7f856f3271d986b3e1f67731e5ff29
igorteg <igor.horuzha@gmail.com>
2020-09-23 22:32:26 +0300

    init

---
+++ src/test/java/Email.java
@@ -0,0 +1,95 @@
+
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Email {
+    private static ChromeDriver driver;
+
+    @BeforeEach
+    public void beforeEachTest() {
+        final String MY_MAIL = "sdfsdfasdf98@yahoo.com";
+
+        final String MY_PASSWORD = "SDFASDFASDF3Q4212A3";
+        System.setProperty("webdriver.chrome.driver", "D:\\MyVisualStudioPrijects\\Old\\SteamProject3 - Copy\\WpfApp1\\bin\\x64\\Debug\\netcoreapp3.1\\chromedriver.exe");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("force-device-scale-factor=0.75");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://login.yahoo.com/?.src=ym&.lang=ru-RU&.intl=ru&.done=https%3A%2F%2Fmail.yahoo.com%2Fd%3Fpspid%3D2023538075%26activity%3Dybar-mail");
+
+
+        sendKeysToElement(By.id("login-username"), MY_MAIL);
+        clickOnElement(By.id("login-signin"));
+        sendKeysToElement(By.id("login-passwd"), MY_PASSWORD);
+        clickOnElement(By.id("login-signin"));
+
+        clickOnElement(By.cssSelector("a[data-test-id=\"compose-button\"]"));
+
+        sendKeysToElement(By.id("message-to-field"), MY_MAIL);
+
+
+    }
+
+    private void clickOnElement(By selector) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                WebElement logInBtn = driver.findElement(selector);
+                logInBtn.click();
+                return;
+            } catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        Assertions.fail();
+    }
+
+    private void sendKeysToElement(By selector, String text) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                WebElement messageTheme = driver.findElement(selector);
+                messageTheme.sendKeys(text);
+                return;
+            } catch (org.openqa.selenium.StaleElementReferenceException ex) {
+
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        Assertions.fail();
+    }
+
+    //    @AfterEach
+//    public void quitDriver() {
+//        driver.quit();
+//    }
+    @Test
+    public void testSendMail() {
+
+
+    }
+}
